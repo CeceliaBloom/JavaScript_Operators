8059ba404433cb5564d1c834909baf6bf845f954
Cecelia Bloom <crbloom01@gmail.com>
2023-12-07 11:44:37 -0500

    JavaScript Operators exercise finished

--- app.js
+++ app.js
@@ -3,17 +3,57 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+          // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a=20;
+let b=4;
+let add=a+b;
+let minus=a-b;
+let multiply=a*b;
+let dividing= a/b;
 
-// Exercise 2
+console.log(
+  add,
+  minus,
+  multiply,
+  dividing);
+
+
+          // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
-// Exercise 3
+console.log(
+  num+str,
+  num+str2,
+  num+isPresent,
+  firstName + num,
+  isPresent + str,
+  firstName + lastName);
+
+
+          // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+console.log(
+  val == str3,
+  val === str3,
+  !isPresent2,
+  ("eleven" == str4 && val >= str3),
+  (!isPresent2 || isPresent2),
+  0 == false,
+  0 === false,
+  0 != false,
+  0 !== false);
